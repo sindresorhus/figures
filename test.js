71555385fa0ad6b32ed85323d0fd1fb30524fae9
Sindre Sorhus <sindresorhus@gmail.com>
2015-11-20 03:35:32 +0700

    minor tweaks

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
 import test from 'ava';
 import figures from './';
 
-console.log('  ' + Object.keys(figures).map(function (el) {
-	return figures[el];
-}).join('  ') + '\n');
+console.log('  ' + Object.keys(figures).map(x => figures[x]).join('  ') + '\n');
 
 test('returns a figure', t => {
 	t.true(figures.tick === '✔' || figures.tick === '√');
