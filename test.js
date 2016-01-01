4ab1ebe36ea299f1d86ff886bd21f01e27725ac6
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 14:02:31 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -5,5 +5,4 @@ console.log('  ' + Object.keys(figures).map(x => figures[x]).join('  ') + '\n');
 
 test('returns a figure', t => {
 	t.true(figures.tick === '✔' || figures.tick === '√');
-	t.end();
 });
