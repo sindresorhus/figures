eea2e0911be64a2b41884c1d6021966b59583d8f
Richie Bendall <richiebendall@gmail.com>
2020-01-16 05:33:31 +1300

    chore: Use spread syntax
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- makefile.js
+++ makefile.js
@@ -14,14 +14,14 @@ const win32 = load('win32');
 
 const data = Object.entries(darwin).map(([name, figure]) => [name, figure, win32[name]]);
 
-// TODO: Use spread syntax when targeting Node 8.3
 const jsonTable = [
 	[
 		'Name',
 		'Real OSes',
 		'Windows'
-	]
-].concat(data);
+	],
+	...data
+]
 
 const figureTable = table(jsonTable, {
 	align: [
