a51c6313792bbf67661c0e2d84b758f87d406c97
ehmicky <ehmicky@users.noreply.github.com>
2021-03-24 17:34:31 +0100

    Add a test validating figures are non-empty strings (#51)

--- test.js
+++ test.js
@@ -21,3 +21,15 @@ test('exported sets', t => {
 	t.is(figures.main.tick, '✔');
 	t.is(figures.windows.tick, '√');
 });
+
+const NON_FIGURE_KEYS = new Set(['main', 'windows']);
+
+test('figures are non-empty strings', t => {
+	for (const [key, figure] of Object.entries(figures)) {
+		if (NON_FIGURE_KEYS.has(key)) {
+			continue;
+		}
+
+		t.true(typeof figure === 'string' && figure.trim() !== '');
+	}
+});
