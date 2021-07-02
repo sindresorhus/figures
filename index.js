24d48d07e6d22bc4a101781345dcee1feac25dcd
ehmicky <ehmicky@users.noreply.github.com>
2021-07-02 21:12:31 +0200

    Clarify `replaceSymbols()` (#88)

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ const shouldUseMain = isUnicodeSupported();
 const figures = shouldUseMain ? mainSymbols : windowsSymbols;
 export default figures;
 
-const isWindowsSymbol = (key, mainSymbol) => figures[key] !== mainSymbol;
+const isWindowsSymbol = (key, mainSymbol) => windowsSymbols[key] !== mainSymbol;
 const getFigureRegExp = (key, mainSymbol) => [new RegExp(escapeStringRegexp(mainSymbol), 'g'), windowsSymbols[key]];
 
 let replacements = [];
